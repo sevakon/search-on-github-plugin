4445884c1778370d623dd4a6ec50122404dd6c04
Vsevolod Konyakhin <sevakonyakhin@gmail.com>
2020-06-11 13:22:30 +0300

    add github icon

--- src/main/java/ru/seva/actions/GitHubSearchAction.java
+++ src/main/java/ru/seva/actions/GitHubSearchAction.java
@@ -1,6 +1,6 @@
 package ru.seva.actions;
 
-//import ru.seva.utils.IconUtil;
+import ru.seva.utils.IconUtil;
 
 public class GitHubSearchAction extends SearchAction {
     private static final String TEXT = "Search on GitHub";
@@ -10,8 +10,6 @@ public class GitHubSearchAction extends SearchAction {
     private static final String URL_POSTFIX = "&type=Code";
 
     public GitHubSearchAction() {
-        //IconUtil.GITHUB_LOGO
-        super(TEXT, DESCRIPTION, null, URL_PREFIX, URL_POSTFIX);
-
+        super(TEXT, DESCRIPTION, IconUtil.GITHUB_LOGO, URL_PREFIX, URL_POSTFIX);
     }
 }
